14eaeea58f3f7173fdd5ebee1cd1fea332fdaa5e
xiaohui.ding <xiaohui.ding@weimob.com>
2020-08-12 18:40:43 +0800

    去掉了jquery依赖

--- delWeibo.js
+++ delWeibo.js
@@ -5,39 +5,35 @@
 // @description  批量删除新浪微博
 // @author       plain
 // @match        https://weibo.com/*/profile?*
-// @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js
-// @requier      https://cdn.bootcdn.net/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js
 // @grant        none
 // ==/UserScript==
 
-jQuery.noConflict();
-(function($) {
+(function() {
     'use strict';
 
     function del() {
         for (var i = 0; i < 100; i++) {
             setTimeout(function() {
-                $('a[action-type="fl_menu"]')[0].click();
-                $('a[title="删除此条微博"]')[0].click();
-                $('a[action-type="ok"]')[0].click();
+                document.querySelector('a[action-type="fl_menu"]').click();
+                document.querySelector('a[title="删除此条微博"]').click();
+                document.querySelector('a[action-type="ok"]').click();
             }, 1000 * i);
         }
     }
 
     // 判断如果有cookie 就不再弹窗，直接进行删除微薄
-    if($.cookie('delWeibo')) {
+    if(document.cookie.indexOf('delWeibo=1') !== -1) {
         del();
     } else {
         var r = confirm("确定好要删除所有微薄了吗，确定好了就点确定吧!");
         if (r === true) {
-            $.cookie('delWeibo', '1');
+            document.cookie = 'delWeibo=1';
             del();
         }
     }
 
     // 关闭浏览器或者标签删除保存的cookie
     window.onbeforeunload = function() {
-        $.removeCookie('delWeibo');
+        document.cookie = 'delWeibo=';
     }
-    
-})(jQuery);
+})();
--- delWeiboFav.js
+++ delWeiboFav.js
@@ -5,40 +5,37 @@
 // @description  批量取消新浪微博收藏
 // @author       plain
 // @match        https://weibo.com/fav*
-// @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js
-// @requier      https://cdn.bootcdn.net/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js
 // @grant        none
 // ==/UserScript==
 
-jQuery.noConflict();
-(function($) {
+(function() {
     'use strict';
 
 	function fav() {
         if (r === true){
             for (var i = 0; i < 100; i++) {
                 setTimeout(function() {
-                    $('a[action-type="fl_favorite"]')[0].click();
-                    $('a[action-type="ok"]')[0].click();
+                    document.querySelector('a[action-type="fl_favorite"]').click();
+                    document.querySelector('a[action-type="ok"]').click();
                 }, 1000 * i);
             }
         }
     }
 
     // 判断如果有cookie 就不再弹窗，直接进行删除微薄
-    if($.cookie('delFav')) {
+    if(document.cookie.indexOf('delFav=1') !== -1) {
         fav();
     } else {
         var r = confirm("确定好要取消所有新浪微博收藏了吗？确定好了就点确定吧!");
         if (r === true) {
-            $.cookie('delFav', '1');
+            document.cookie = 'delFav=1';
             fav();
         }
     }
 
     // 关闭浏览器或者标签删除保存的cookie
     window.onbeforeunload = function() {
-        $.removeCookie('delFav');
+        document.cookie = 'delFav=';
     }
 
-})(jQuery);
+})();
